cd095df921b518fc0a8cd5b1bd32a564780b4fe7
caglarsarikaya <caglarsarikaya@users.noreply.github.com>
2026-03-20 10:38:23 +0000

    deploy: b058140815c550b783970d1e444a0c4c15f60f86

--- components.js
+++ components.js
@@ -3,14 +3,19 @@
 // 1. Shared Head Tag Imports (Fonts & Google Analytics)
 function injectGlobalHead() {
   const head = document.head;
+  
+  // Add font preconnects and stylesheet
   head.insertAdjacentHTML('beforeend', `
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@400;600&family=Source+Sans+3:ital,wght@0,400;0,600;1,400&display=swap" rel="stylesheet">
-    
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-6DJCPL0T9F"></script>
   `);
+
+  // Google Analytics - must use createElement for script to execute
+  const gtagScript = document.createElement('script');
+  gtagScript.async = true;
+  gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-6DJCPL0T9F';
+  head.appendChild(gtagScript);
 
   window.dataLayer = window.dataLayer || [];
 
